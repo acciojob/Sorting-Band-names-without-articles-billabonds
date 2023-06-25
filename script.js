b37e4c5da8b942326e13763c4e072a254503dee1
Shivam <73821061+billabonds@users.noreply.github.com>
2023-06-25 11:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+// let n = touristSpots.length;
+
+// for(let i=0;i<n;i++)
+// {
+	
+// }
+
+console.log(touristSpots.sort());
