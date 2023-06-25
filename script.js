c6f2c59cc46b9ccb4428abd1ae02bd3cd7bb391c
Shivam <73821061+billabonds@users.noreply.github.com>
2023-06-25 12:25:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-// let n = touristSpots.length;
-
-// for(let i=0;i<n;i++)
-// {
-	
-// }
-
 console.log(touristSpots.sort());
+
+console.log(touristSpots.sort((a,b) =>{
+
+	list1 = a.split(" ");
+	str1 = list1.filter((item) => item != "The" && item != "A" && item != "An");
+
+	list2 = b.split(" ");
+	str2 = list2.filter((item) => item != "The" && item != "A" && item != "An");
+
+	if(str1 < str2){return -1;}
+	if(str1 > str2){return 1;}
+	return 0;
+}));
